bc36f66e622e93c1cb8b0442ade124ef025aacae
dominick.unsworth <domxx9@gmail.com>
2018-12-21 11:34:49 +0000

    added some notes and fixed some gramma things

--- Flex Calc.py
+++ Flex Calc.py
@@ -6,7 +6,7 @@ import calendar as ca
 
 # --------------------Functions--------------------#
 
-def lunch_func(t):
+def lunch_func(t):  # Determines the amount of time is given as lunch
     if t == "1":
         return str("0030")
     elif t == "2":
@@ -19,18 +19,18 @@ def lunch_func(t):
         exit("Please enter a valid lunch option!")
 
 
-def time_func(a):
+def time_func(a):  # Converts HHMM string in to timedelta
     user_hours = int(a[:2])
     user_minute = int(a[2:5])
     b = td(hours=user_hours, minutes=user_minute)
     return b
 
 
-def if_func(a, b):
-    if a == "1":
-        afternoon_time = td(hours=int(3), minutes=int(45))
-    if a == "2":
+def if_func(b, c):  # This is what enable you to check flex before 1:45 and 9am
+    if c.hour < 9:
         afternoon_time = td(hours=int(7), minutes=int(30))
+    else:
+        afternoon_time = td(hours=int(3), minutes=int(45))
     balance = b
     lunch = lunch_func(input("Set time amount for lunch: '1' = 30 min, '2' = 45 min, '3' = 1 hr, '4' = 1 hr 30 min:"))
     flav = time_func(flex) + time_func(lunch) + afternoon_time
@@ -38,10 +38,10 @@ def if_func(a, b):
     return lunch, flav, balance
 
 
-def custom_time_func():
-    q = input("Do you want to enter a custom time Y/N :")
+def custom_time_func():  # This allows a custom time to be used for debuging purposes .
+    q = input("Do you want to enter a custom time Y/N : ")
     if q == "y" or q == "Y":
-        a = input("Enter a custom time in HHMM format:")
+        a = input("Enter a custom time in HHMM format : ")
         user_hours = int(a[:2])
         user_minute = int(a[2:5])
         diff = 60 * 60 * 24
@@ -69,9 +69,7 @@ flav = time_func(flex)
 
 
 if ((now.hour <= 13 and now.minute <= 44) or (now.hour <= 12 and now.minute >= 45)) and now.hour >= 9:
-    lunch, flav, balance = if_func("1",balance)
-if now.hour < 9:
-    lunch, flav, balance = if_func("2", balance)
+    lunch, flav, balance = if_func(balance, now)
 
 # --------------------Code and Printing--------------------#
 
@@ -97,4 +95,3 @@ print("Time when at +0:30 : "+str(balance + time_func(static_flex[2])))
 print("Time when at -0:30 : "+str(balance - time_func(static_flex[2])))
 print("Time when at -1:00 : "+str(balance - time_func(static_flex[1])))
 print("Time when at -2:00 : "+str(balance - time_func(static_flex[0])))
-
