65620ac13ee2e161bee43fe72fb5104a15954f7b
Dominick Unsworth <domxx9@gmail.com>
2019-01-22 16:50:04 +0000

    Amended so all flex values don't print out and changed the values for the interface

---
+++ discord bot/Discord bot.py
@@ -0,0 +1,8 @@
+from discord.ext import commands
+import discord
+
+bot = commands.Bot(command_prefix="!", status=discord.Status.idle, activity=discord.Game(name="Booting.."))
+
+bot.Run(NTM3MDE0MjAzMzc5MjIwNDgw.DyfHcg.SFErGhRLjqMl5zlkSNucq1pXmrg)
+
+
